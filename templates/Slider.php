dd17a7a4c65593d67b378a9dd7cd865a51794bd8
chienphan23 <chienphan230702@gmail.com>
2023-12-18 09:13:19 +0700

    first commit

---
+++ templates/Slider.php
@@ -0,0 +1,70 @@
+<div class="sidebar">
+        <li>
+        <a id="side--bar">Màu</a>
+        <form action="/home/filter" method="post">
+
+            <button type="submit" id="side-bar" >Red</button>
+            <input type="hidden" name="mauInput" value="red">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Orange</button>
+            <input type="hidden" name="mauInput" value="orange">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Black</button>
+            <input type="hidden" name="mauInput" value="black">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">White</button>
+            <input type="hidden" name="mauInput" value="white">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Yellow</button>
+            <input type="hidden" name="mauInput" value="yellow">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Blue</button>
+            <input type="hidden" name="mauInput" value="blue">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Grey</button>
+            <input type="hidden" name="mauInput" value="grey">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Purple</button>
+            <input type="hidden" name="mauInput" value="purple">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Pink</button>
+            <input type="hidden" name="mauInput" value="pink">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Green</button>
+            <input type="hidden" name="mauInput" value="green">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Brown</button>
+            <input type="hidden" name="mauInput" value="brown">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">Gold</button>
+            <input type="hidden" name="mauInput" value="Gold">
+        </form>
+        <form action="/home/filter" method="post">
+
+            <button id="side-bar" href="#home">silver</button>
+            <input type="hidden" name="mauInput" value="silver">
+        </form>
+        </li>
+    </div>
